c842af38ea01a2f333bb9e7877cc2f8b5e7f70fb
Jacob Skwirsk <skwirskj@umich.edu>
2024-12-06 21:46:16 -0500

    Generated ground truth data

--- generate_nuscenes_semantic_seg_gt.py
+++ generate_nuscenes_semantic_seg_gt.py
@@ -1,11 +1,47 @@
 import numpy as np
 from nuscenes import NuScenes
 from nuscenes.utils.color_map import get_colormap
+from PIL import Image
+
+# Panoptic labels for nuscenes
+NUSC_ID_2_NAME = {0: 'noise',
+ 1: 'animal',
+ 2: 'human.pedestrian.adult',
+ 3: 'human.pedestrian.child',
+ 4: 'human.pedestrian.construction_worker',
+ 5: 'human.pedestrian.personal_mobility',
+ 6: 'human.pedestrian.police_officer',
+ 7: 'human.pedestrian.stroller',
+ 8: 'human.pedestrian.wheelchair',
+ 9: 'movable_object.barrier',
+ 10: 'movable_object.debris',
+ 11: 'movable_object.pushable_pullable',
+ 12: 'movable_object.trafficcone',
+ 13: 'static_object.bicycle_rack',
+ 14: 'vehicle.bicycle',
+ 15: 'vehicle.bus.bendy',
+ 16: 'vehicle.bus.rigid',
+ 17: 'vehicle.car',
+ 18: 'vehicle.construction',
+ 19: 'vehicle.emergency.ambulance',
+ 20: 'vehicle.emergency.police',
+ 21: 'vehicle.motorcycle',
+ 22: 'vehicle.trailer',
+ 23: 'vehicle.truck',
+ 24: 'flat.driveable_surface',
+ 25: 'flat.other',
+ 26: 'flat.sidewalk',
+ 27: 'flat.terrain',
+ 28: 'static.manmade',
+ 29: 'static.other',
+ 30: 'static.vegetation',
+ 31: 'vehicle.ego'}
 
 
 def generate_nuscenes_semantic_segmentation_gt():
     SENSOR_CHANNEL = 'CAM_FRONT'
     POINT_SENSOR_CHANNEL = 'LIDAR_TOP'
+    OUTPUT_DIR = 'datasets/nuscenes/sem_seg_samples_gt/CAM_FRONT'
     nusc = NuScenes(version='v1.0-mini', dataroot='datasets/nuscenes', verbose=True)
     nuscenes_name2idx_map = nusc.lidarseg_name2idx_mapping
     nuscenes_idx2name_map = nusc.lidarseg_idx2name_mapping
@@ -26,6 +62,8 @@ def generate_nuscenes_semantic_segmentation_gt():
             sample = nusc.get('sample', sample_token)
             pointsensor_token = sample['data'][POINT_SENSOR_CHANNEL]
             camera_token = sample['data'][SENSOR_CHANNEL]
+            sample_image_path = nusc.get('sample_data', camera_token)['filename']
+            sample_image_filename = sample_image_path.split('/')[-1]
 
             # Map the point cloud to the image, im is the image without semantic labels on it
             points, coloring, im = nusc.explorer.map_pointcloud_to_image(pointsensor_token, camera_token,
@@ -49,11 +87,11 @@ def generate_nuscenes_semantic_segmentation_gt():
             """
 
             # Points[0,:] and points[1,:] are the x and y coordinates of the points in the image of where the semantic labels are
-            cam_front_x, cam_front_y = points[0,:], points[1,:]
+            cam_front_x, cam_front_y = points[1,:], points[0,:]
 
             # each entry in coloring corresponds to a point in the point cloud, so coloring[0,:] is the RGB (or BGR) value of the first point in points
             # Numpy array that is grayscale, all zeros to start. Each number is a label associated with the pixel.
-            segmentation_ground_truths = np.zeros((im.size[0], im.size[1]), dtype=np.uint8)
+            segmentation_ground_truths = np.zeros((im.size[1], im.size[0]), dtype=np.uint8)
             
             # For each entry in the coloring, we need to convert the RGB value to a label
             for i in range(coloring.shape[0]):
@@ -67,6 +105,16 @@ def generate_nuscenes_semantic_segmentation_gt():
                 if current_label is None:
                     print(f"Did not find the label for the color: {current_label}")
                 
+                # Get the nuScenes integer for the label
+                current_name2idx = nuscenes_name2idx_map[current_label]
+
+                # Set the label in the segmentation_ground_truths where the point is
+                segmentation_ground_truths[int(cam_front_x[i]), int(cam_front_y[i])] = current_name2idx
+            
+            # Save the ground truth to the output directory
+            gt_path = f"{OUTPUT_DIR}/{sample_image_filename}"
+            segmentation_ground_truths = Image.fromarray(segmentation_ground_truths)
+            segmentation_ground_truths.save(gt_path)
             sample_token = sample['next']
         
 
